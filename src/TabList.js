48ec12db00879720dabf6cdca57668cefbb1f9d3
Bartlomiej Potaczek <bartlomiej.potaczek7@gmail.com>
2017-05-05 22:32:43 +0200

    indents refactoring

--- src/TabList.js
+++ src/TabList.js
@@ -1,21 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const TabList = ({ children, changeTab, activeTab, styleName }) => {
-  const childrenWithProps = React.Children.map(children,
-    (child, index) => React.cloneElement(child, {
-      index, changeTab, activeTab
-    })
-  )
+const TabList = ({children, changeTab, activeTab, styleName}) => {
+    const childrenWithProps = React.Children.map(children,
+        (child, index) => React.cloneElement(child, {
+            index, changeTab, activeTab
+        })
+    )
 
-  return (<div className={styleName}>{childrenWithProps}</div>)
+    return (<div className={styleName}>{childrenWithProps}</div>)
 }
 
 TabList.propTypes = {
-  children: PropTypes.array,
-  changeTab: PropTypes.func,
-  activeTab: PropTypes.number,
-  styleName: PropTypes.string
+    children: PropTypes.array,
+    changeTab: PropTypes.func,
+    activeTab: PropTypes.number,
+    styleName: PropTypes.string
 }
 
 export default TabList
