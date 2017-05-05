96e411ebc462de8661ce0cf91b3128012e4f5a43
Bartlomiej Potaczek <bartlomiej.potaczek7@gmail.com>
2017-05-05 23:48:23 +0200

    export components

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import _Tab from './Tab'
+import _TabList from './TabList'
+import _TabPanel from './TabPanel'
+import _Tabs from './Tabs'
+
+export default _Tabs
+
+export const Tab = _Tab
+export const TabList = _TabList
+export const TabPanel = _TabPanel
+export const Tabs = _Tabs
