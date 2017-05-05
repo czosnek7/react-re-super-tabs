b7be068a68e7ab9688bf110c53e4cac7452e159b
Bartlomiej Potaczek <bartlomiej.potaczek7@gmail.com>
2017-05-05 23:26:55 +0200

    add Tab tests

--- tests/Tab.js
+++ tests/Tab.js
@@ -1,17 +1,51 @@
 import {expect} from 'chai'
 import React from 'react'
-import {mount, shallow} from 'enzyme'
+import {mount, shallow, render} from 'enzyme'
 
-import {Tabs} from '../src'
+import {Tab} from '../src'
 
-describe('Tabs', () => {
-    it('contains proper initial state', () => {
-        const wrapper = mount(<Tabs />)
-        expect(wrapper.state('activeTab')).to.eql(0)
+describe('Tab', () => {
+    it('component props needs to be a react component function', () => {
+        const component = () => <div>tab 1</div>
+        const wrapper = mount(<Tab component={component}/>)
+
+        expect(wrapper.props().component).to.be.instanceof(Function)
     })
 
-    it('implements changeTab functionality', () => {
-        const wrapper = mount(<Tabs />)
-        //expect(wrapper.node.changeTab).to.not.be.undefined
+    it('has different index property than activeTab property', () => {
+        const component = () => <div>tab 1</div>
+        const wrapper = mount(<Tab {...{component, index: 0, activeTab: 1}}/>)
+
+        expect(wrapper.props().index).to.not.eql(wrapper.props().activeTab)
+    })
+
+    it('has the same index as activeTab property', () => {
+        const component = () => <div>tab 1</div>
+        const wrapper = mount(<Tab {...{component, index: 0, activeTab: 0}}/>)
+
+        expect(wrapper.props().index).to.eql(wrapper.props().activeTab)
+    })
+
+    it('has the same index as activeTab property after props change', () => {
+        const component = () => <div>tab 1</div>
+        const wrapper = mount(<Tab {...{component, index: 0, activeTab: 1}}/>)
+
+        wrapper.setProps({activeTab: 0});
+
+        expect(wrapper.props().index).to.eql(wrapper.props().activeTab)
+    })
+
+    it('has label tab 1 provided by custom component', () => {
+        const component = () => <div>tab 1</div>
+        const wrapper = mount(<Tab {...{component}}/>)
+
+        expect(wrapper.text()).to.eql('tab 1')
+    })
+
+    it('has label tab 1 provided by label property', () => {
+        const component = ({children}) => <div>{children}</div>
+        const wrapper = mount(<Tab {...{component, label: 'tab 1'}}/>)
+
+        expect(wrapper.text()).to.eql('tab 1')
     })
 })
--- tests/index-test.js
+++ tests/index-test.js
@@ -1,2 +1,3 @@
 import Tabs from './Tabs'
 import TabList from './TabList'
+import Tab from './Tab'
