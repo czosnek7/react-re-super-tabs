16d12a55ce8ab7b208b6fe35979239db5c893b30
Bartlomiej Potaczek <bartlomiej.potaczek7@gmail.com>
2017-05-05 22:29:00 +0200

    indents refactoring

--- src/Tabs.js
+++ src/Tabs.js
@@ -2,33 +2,33 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 class TabsContainer extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { activeTab: 0 }
-    this.changeTab = this.changeTab.bind(this)
-  }
+    constructor(props) {
+        super(props)
+        this.state = {activeTab: 0}
+        this.changeTab = this.changeTab.bind(this)
+    }
 
-  render () {
-    const { activeTab } = this.state
-    const childrenWithProps = React.Children.map(this.props.children,
-      (child, index) => React.cloneElement(child, {
-        index, changeTab: this.changeTab, activeTab
-      })
-    )
+    render() {
+        const {activeTab} = this.state
+        const childrenWithProps = React.Children.map(this.props.children,
+            (child, index) => React.cloneElement(child, {
+                index, changeTab: this.changeTab, activeTab
+            })
+        )
 
-    return (<div>{childrenWithProps}</div>)
-  }
+        return (<div>{childrenWithProps}</div>)
+    }
 
-  changeTab (event) {
-    const { currentTarget } = event
-    const activeTab = parseInt(currentTarget.getAttribute('data-tab'))
+    changeTab(event) {
+        const {currentTarget} = event
+        const activeTab = parseInt(currentTarget.getAttribute('data-tab'))
 
-    this.setState(() => ({ activeTab }))
-  }
+        this.setState(() => ({activeTab}))
+    }
 }
 
 TabsContainer.propTypes = {
-  children: PropTypes.array
+    children: PropTypes.array
 }
 
 export default TabsContainer
